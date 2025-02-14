8f9d31134085593a4fd119b84f96f474dcaa3520
Paromita <paromitatalukder04@gmail.com>
2025-02-14 11:19:47 -0500

    added music progress bar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const audio = document.getElementById("background-music");
     const playButton = document.querySelector(".play-music");
+    const progressBar = document.getElementById("progress-bar");
 
     //auto-play on page load
     audio.play();
@@ -14,5 +15,17 @@ document.addEventListener("DOMContentLoaded", function() {
             audio.pause();
             playButton.innerHTML = "&#9658;";  //change to play icon
         }
+    });
+
+    //update progress bar as song plays
+    audio.addEventListener("timeupdate", function () {
+        const progress = (audio.currentTime/audio.duration) * 100;
+        progressBar.value = progress;
+    });
+
+    //users can seek using progress bar 
+    progressBar.addEventListener("input", function (e) {
+        const seektime = (progressBar.value/100) * audio.duration;
+        audio.currentTime = seektime;
     });
 });
